32f01692a3bf0772fdbce80a46ea0bee19bd51f5
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-07-09 09:40:30 -0300

    Exercicio soibre sentença de código e bloco de código

---
+++ fundamentos/exercicio/sentencaBloco.js
@@ -0,0 +1,24 @@
+/**
+ * Imprimar números IMPARES
+ * Imprimar números PARES
+ */
+
+// Números impar
+{
+  console.log("Números impares.");
+  console.log(1);
+  console.log(3);
+  console.log(5);
+  console.log(7);
+  console.log(9);
+}
+
+// Números pares
+{
+  console.log("Números pares.");
+  console.log(2);
+  console.log(4);
+  console.log(6);
+  console.log(8);
+  console.log(10);
+}
