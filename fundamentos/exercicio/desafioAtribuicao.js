c8961e7074bcbfb3f263582f7613fae573484732
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-07-12 20:15:52 -0300

    Desafio operador atribuição.

---
+++ fundamentos/exercicio/desafioAtribuicao.js
@@ -0,0 +1,18 @@
+/**
+ *  Lista de Aprovados
+ *  =====================
+ * 
+ *  1. Ana Silva
+ *  2. Pedro Albuquerque
+ *  3. Guilherme Pereira
+ *  4. Rebeca França
+ */
+
+let texto = "Lista de Aprovados";
+texto += "\n=====================";
+texto += "\n1. Ana Silva";
+texto += "\n2. Pedro Albuquerque";
+texto += "\n3. Guilherme Pereira";
+texto += "\n4. Rebeca França";
+
+console.log(texto);
