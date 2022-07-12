600ef291bb2d947d7926761f52030af637aa3c1b
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-07-12 15:42:49 -0300

    Aula sobre tipos basicos

---
+++ fundamentos/inicio/dados3.js
@@ -0,0 +1,17 @@
+/**
+ * Tipos Básicos em javascript
+ * 
+ * number => números gerais
+ * string => sequencia de letras
+ * boolean => Verdadeiro ou Falso
+ */
+
+let idade = 30; // number
+let salario = 4568.32; // number
+let nome = "w2k"; // string
+let estaSol = true // ou false
+
+console.log(typeof idade);
+console.log(typeof salario);
+console.log(typeof nome);
+console.log(typeof estaSol);
