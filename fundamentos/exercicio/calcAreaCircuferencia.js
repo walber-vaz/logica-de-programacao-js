6bfd56105c65de9204128c3b7c9c890cbde92fb7
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-07-12 15:59:15 -0300

    Exercicio calcuçar area.

---
+++ fundamentos/exercicio/calcAreaCircuferencia.js
@@ -0,0 +1,6 @@
+// Calcule area da circunferência.
+const PI = 3.14;
+let valorDeRaio = 10;
+let valorDaArea = PI * valorDeRaio * valorDeRaio;
+
+console.log(`O resultado da area é: ${valorDaArea}m²`);
