33e660caf71d576f27c3297d11aacf72d4e358ac
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-07-12 19:17:35 -0300

    Aula operador de igualdade estrita e diferança estrita.

---
+++ fundamentos/operadores/relacionais2.js
@@ -0,0 +1,11 @@
+let a = 1;
+let b = "1";
+
+// == igual so valores iguais.
+console.log(a == b);
+// Comparação restrita => tanto tipo quanto o valor devem ser igual.
+console.log(a === b);
+
+console.log(a != b);
+// diferençá restrita
+console.log(a !== b);
