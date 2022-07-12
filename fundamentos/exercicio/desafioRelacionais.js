62cee3524ca5fd967d2b448c30f7a421ab5cc98b
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-07-12 19:33:59 -0300

    Desafio operadores relacionais.

---
+++ fundamentos/exercicio/desafioRelacionais.js
@@ -0,0 +1,16 @@
+let a = 1;
+let b = 2;
+
+// Encontre 5 expressões que o resultado é TRUE
+console.log(a < b);
+console.log(a != b);
+console.log(b > a);
+console.log(b != a);
+console.log(b >= a);
+
+// Encontre 5 expressões que o resultado é FALSE
+console.log(a > b);
+console.log(a == b);
+console.log(b < a);
+console.log(b <= a);
+console.log(a >= b);
