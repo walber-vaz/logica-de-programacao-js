c501e8c2013f595667d3ccd3ae568b0f9fc13925
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-07-13 17:36:24 -0300

    Aula sobre operadores unários

---
+++ fundamentos/operadores/operadoresUnarios.js
@@ -0,0 +1,16 @@
+/**
+ * Operadores Unários
+ * ==================
+ * 
+ * ! -> negação
+ * ++ -> incremento antes ou depois do incremento Ex: a++, ++a
+ * -- -> decremento antes ou depois do incremento Ex: a--, --a
+ */
+
+let a = 1;
+a++;
+++a;
+
+a--;
+--a;
+console.log(a);
