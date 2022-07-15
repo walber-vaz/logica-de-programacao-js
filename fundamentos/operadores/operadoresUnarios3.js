284e0dab6d279d354b2b40cd6f481230ef6dc5fc
Walber Vaz da Silva <wvs.walber@gmail>
2022-07-15 09:55:20 -0300

    Aula sobre operadores unarios 3

---
+++ fundamentos/operadores/operadoresUnarios3.js
@@ -0,0 +1,8 @@
+// Operador negação lógico
+console.log(!!true);
+console.log(!!false);
+
+
+// Converte textos em TREU e FALSE: !!(dupla negação)
+// Todo números convertido para verdadeiro ou falso e VERDADEIDO
+// menos o 0 que é FALSO.
