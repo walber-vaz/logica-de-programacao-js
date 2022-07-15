8a0e27d0ceb08c3365201248d3789dd54d99e308
Walber Vaz da Silva <wvs.walber@gmail>
2022-07-15 16:21:44 -0300

    desafio estrutura IF

---
+++ fundamentos/exercicio/desafioIf.js
@@ -0,0 +1,21 @@
+const nota = 9;
+
+if (nota >= 9 && nota <= 10) {
+  console.log('A - Passou de ano');
+}
+
+if (nota >= 7 && nota <= 8.9) {
+  console.log('B - Passou mais melhore');
+}
+
+if (nota >= 5 && nota <= 6.9) {
+  console.log('C - Hmmm!! tem que melhorar');
+}
+
+if (nota >= 4.5 && nota <= 4.9) {
+  console.log('D - Poxa, ta ruin em!!');
+}
+
+if (nota >= 0 && nota < 4.5) {
+  console.log('F - Pena tente de novo.');
+}
