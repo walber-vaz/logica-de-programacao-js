c71fb25a97a6d0ca99ab2acd048dfef039f3db66
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-07-20 14:01:31 -0300

    Aula sobre break e continue

---
+++ fundamentos/estruturaControle/breakContinue.js
@@ -0,0 +1,15 @@
+// Break
+for (let i = 1; i <= 5; i++) {
+  if (i === 3) {
+    break;
+  }
+  console.log(i);
+}
+
+// Continue
+for (let i = 1; i <= 5; i++) {
+  if (i === 3) {
+    continue;
+  }
+  console.log(i);
+}
