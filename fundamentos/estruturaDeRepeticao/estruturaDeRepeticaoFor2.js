fd96cf47684cafc65ccd420c1869ef191483d4a9
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-07-20 09:23:00 -0300

    Aula sobre laço for

---
+++ fundamentos/estruturaDeRepeticao/estruturaDeRepeticaoFor2.js
@@ -0,0 +1,13 @@
+// Laço infinito com for
+// for (;;) {}
+
+// for (; true; ) {}
+
+let controle = 1;
+
+for (; controle <= 10; ) {
+  console.log(controle);
+  controle++;
+}
+
+console.log('Fim');
---
+++ fundamentos/estruturaDeRepeticao/estruturaDeRepeticaoFor3.js
@@ -0,0 +1,4 @@
+// Imprimar valro de 7 a 3
+for (let i = 7; i >= 3; i--) {
+  console.log(i);
+}
