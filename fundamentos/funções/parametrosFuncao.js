b0c9fa9dd89c63fcec3b3419ccef5b6b6ec8de7b
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-08-09 21:42:06 -0300

    Aula função - passando parametro meno paramentro para uma função

---
+++ fundamentos/funções/parametrosFuncao.js
@@ -0,0 +1,8 @@
+function somar(a, b, c, d) {
+  return a + b + c + d;
+}
+
+// Se uma função recebe 4 parametro e voce manda apenas 3
+// O parametro não passado recebe 'undefined'
+// Não gera erro para codigo.
+console.log(somar(1, 2, 3));
