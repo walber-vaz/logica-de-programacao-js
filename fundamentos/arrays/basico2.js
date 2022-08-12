5135c36fc7992410ca7e344232b625dafa78b126
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-08-12 20:41:06 -0300

    Aula Array - Basico de array colocando indice em um array vazio

---
+++ fundamentos/arrays/basico2.js
@@ -0,0 +1,11 @@
+const nomes = [];
+
+// Colocando os indice no array vazio
+nomes[0] = 'Pedro';
+nomes[1] = 'Jubileu';
+nomes[2] = 'Ana';
+nomes[3] = 'Rebeca';
+nomes[4] = 'Maria';
+nomes[10] = 'Mariazinha';
+
+console.table(nomes);
