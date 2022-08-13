e7e32ef0c83499024ca830339132340e4606b142
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-08-12 21:23:29 -0300

    Desafio de array - percorre indice do array aparti do penultimo indice

---
+++ fundamentos/exercicio/desafioArray.js
@@ -0,0 +1,6 @@
+// Percorra esse array apartir do penultimo elemento do array
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = numeros.length - 2; i >= 0; i -= 2) {
+  console.log(numeros[i]);
+};
