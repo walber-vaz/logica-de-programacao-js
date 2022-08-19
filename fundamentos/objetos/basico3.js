f566096b8d3211529abef33a576cef7275c755cd
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-08-19 11:13:14 -0300

    Aula Objetos - Alterando atributos de objeto

---
+++ fundamentos/objetos/basico3.js
@@ -0,0 +1,11 @@
+const data = {
+    dia: 1,
+    mes: 10,
+    ano: 2022
+};
+
+// alterar valor de um atributo dentro de objeto
+data.dia = 27;
+data.mes = 12;
+
+console.log(`A data atual: ${data.dia}/${data.mes}/${data.ano}`);
