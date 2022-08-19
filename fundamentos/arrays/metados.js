ed00b83891849af04fd0285b8adf7ac8efa046c0
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-08-19 08:29:23 -0300

    Aula array - metados

---
+++ fundamentos/arrays/metados.js
@@ -0,0 +1,12 @@
+const numeros = [1, 2, 3, 4, 5];
+
+// Trocar valor do indice do array na posição 0
+numeros[0] = 100;
+
+// Adicionar na ultima posição do array 
+numeros.push(6);
+
+// junto dos os indice do array com ,
+console.log(numeros.join(','))
+
+console.log(numeros);
