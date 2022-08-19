37505aee2373aef0aba28bf279a52bf2dc9eafff
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-08-19 14:26:58 -0300

    Aula Objetos - Problema em fazer objetos de forma literal

---
+++ fundamentos/objetos/problemaFormaLiteral.js
@@ -0,0 +1,21 @@
+const d1 = {
+    dia: 22,
+    mes: 11,
+    ano: 2022,
+    exibirData: function () {
+        return `${this.dia}/${this.mes}/${this.ano}`
+    }
+}
+
+const d2 = {
+    dia: 23,
+    mes: 11,
+    ano: 2022,
+    exibirData: function () {
+        return `${this.dia}/${this.mes}/${this.ano}`
+    }
+}
+
+/*
+    Erro de repetição de código
+*/
