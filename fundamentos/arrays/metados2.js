00ff63ed3a32149bc30b87ecee42a29fb554d07c
Walber Vaz da Silva <wvs.walber@gmail.com>
2022-08-19 08:43:57 -0300

    Aula array - metados - 2

---
+++ fundamentos/arrays/metados2.js
@@ -0,0 +1,11 @@
+const numeros = [1, 2, 3, 4, 5];
+
+// Remove elementos dentro de um array
+numeros.splice(1, 2)
+
+console.log(numeros)
+
+// Remover ultimo elemento do array e mostra 
+console.log(numeros.pop())
+console.log(numeros.pop())
+console.log(numeros)
